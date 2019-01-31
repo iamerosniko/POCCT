5d4ac23fda5891125e6f88cc0169fcd3da6a8904
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2019-01-31 14:24:09 -0500

    up

--- CTAFrontEnd/src/app/services/btamservice.service.ts
+++ CTAFrontEnd/src/app/services/btamservice.service.ts
@@ -1,31 +1,31 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
-import { ClientApiService } from './clientapi.service'; 
-import { ClientApiSettings } from './clientapi.settings'; 
+import { ClientApiService } from "./clientapi.service";
+import { ClientApiSettings } from "./clientapi.settings";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class BtamserviceService {
-
-  constructor(private api:ClientApiService) {
+  constructor(private api: ClientApiService) {
     api.normalHeader();
   }
 
-  logIn(){
-    
+  logIn() {
     this.api.normalHeader();
     //dev-debug mode
-    // this.api.apiUrl=ClientApiSettings.GETCLIENTAPIURL("btamconnection/appsignin/alverer@mfcgd.com")
+    this.api.apiUrl=ClientApiSettings.GETCLIENTAPIURL("btamconnection/appsignin/alverer@mfcgd.com")
     //production - actual mode
-    this.api.apiUrl=ClientApiSettings.GETCLIENTAPIURL("btamconnection/appsignin")
-    var res= this.api.getAll();
+    // this.api.apiUrl = ClientApiSettings.GETCLIENTAPIURL("btamconnection/appsignin");
+    var res = this.api.getAll();
     return res;
   }
 
-  getUsers(){
+  getUsers() {
     this.api.normalHeader();
-    this.api.apiUrl=ClientApiSettings.GETCLIENTAPIURL("btamconnection/GetUsers")
-    var res= this.api.getAll();
+    this.api.apiUrl = ClientApiSettings.GETCLIENTAPIURL(
+      "btamconnection/GetUsers"
+    );
+    var res = this.api.getAll();
     return res;
   }
 }
--- CTAFrontEnd/src/app/services/clientapi.settings.ts
+++ CTAFrontEnd/src/app/services/clientapi.settings.ts
@@ -1,27 +1,27 @@
 export class ClientApiSettings {
-    //if NOT using ng serve
-    // private static CURRENT_URL = "api/"
-    //if using NG Serve
-//localhost
-    // private static CLIENT_URL = "http://localhost:57485/api/"
-//direct     
-    // private static CLIENT_URL = "https://calltracker.apps.cac.preview.pcf.manulife.com/api/"
-//prod build
-    private static CLIENT_URL = "api/"
-    
-    // private static BW_URL = "http://localhost:64112/api/"
-    private static BW_URL =""
+  //if NOT using ng serve
+  // private static CURRENT_URL = "api/"
+  //if using NG Serve
+  //localhost
+  // private static CLIENT_URL = "http://localhost:57485/api/"
+  //direct
+  private static CLIENT_URL =
+    "https://calltracker.apps.cac.preview.pcf.manulife.com/api/";
+  //prod build
+  // private static CLIENT_URL = "api/"
 
-    public static GETBWURL(controller:string):string{
-        return this.BW_URL+controller;
-    }
+  private static BW_URL = "";
 
-    public static GETCLIENTAPIURL(controller:string):string{
-        return this.CLIENT_URL+controller;
-    }
+  public static GETBWURL(controller: string): string {
+    return this.BW_URL + controller;
+  }
 
-    public static HANDLEERROR(error : any):Promise<any>{
-        console.error('An error occured',error);
-        return Promise.reject(error.message || error);
-    }
+  public static GETCLIENTAPIURL(controller: string): string {
+    return this.CLIENT_URL + controller;
+  }
+
+  public static HANDLEERROR(error: any): Promise<any> {
+    console.error("An error occured", error);
+    return Promise.reject(error.message || error);
+  }
 }
